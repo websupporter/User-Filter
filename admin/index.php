ec2d8abff65d96ceff4aadc558056f3411cf1145
websupporter <webmaster@websupporter.net>
2016-08-24 12:23:34 +0300

    Add basic table for browsing the filters

--- admin/index.php
+++ admin/index.php
@@ -9,16 +9,25 @@
 ?><div class="wrap">
 	<h1><?php _e( 'Advanced User Search', 'auf' ); ?></h1>
 
-	<a class="button" href="<?php echo esc_url( add_query_arg( array( 'new' => 1 ) ) ); ?>"><?php _e( 'Create a new filter' ); ?></a>
+	<a class="button" href="<?php echo esc_url( add_query_arg( array( 'new' => 1 ) ) ); ?>"><?php _e( 'Create a new filter', 'auf' ); ?></a>
 
 	<?php if( ! $filters ) : ?>
 	<p><?php _e( 'No filters found.', 'auf' ); ?></p>
 	<?php else: ?>
-
-	<table class="table">
+	<!-- Maybe later via the table_class -->
+	<table class="wp-list-table widefat fixed striped posts">
 		<thead>
+			<tr>
+				<th>ID</th><th>Name</th>
+			</tr>
 		</thead>
 		<tbody>
+			<?php foreach ( $filters as $filter ): ?>
+			<tr>
+				<td><a href="<?php echo esc_url( add_query_arg( array( 'ID' => $filter['ID'] ) ) ); ?>" title="<?php echo esc_attr( __( 'Edit filter', 'auf' ) ); ?>"><?php echo $filter['ID']; ?></td>
+				<td><?php echo $filter['name']; ?></td>
+			</tr>
+			<?php endforeach; ?>
 		</tbody>
 	</table>
 
@@ -29,4 +38,4 @@
 		 * Fires after the admin page gets rendered
 		 **/
 		do_action( 'auf::admin::after' );
-?><
+?>
